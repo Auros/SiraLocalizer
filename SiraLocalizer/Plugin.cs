34f8660edd317e34ac285f9664b09fa4c47fcfd6
Nicolas Gnyra <nicolas@gnyra.com>
2020-10-29 16:39:46 -0400

    Fix TextSegmentedControlCell text alignment

--- SiraLocalizer/Plugin.cs
+++ SiraLocalizer/Plugin.cs
@@ -1,6 +1,8 @@
-﻿using IPA;
+﻿using HarmonyLib;
+using IPA;
 using SiraUtil.Zenject;
 using SiraLocalizer.Installers;
+using System.Reflection;
 using IPALogger = IPA.Logging.Logger;
 
 namespace SiraLocalizer
@@ -8,17 +10,33 @@ namespace SiraLocalizer
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
+        private const string kHarmonyId = "dev.auros.siralocalizer";
+
         internal static IPALogger Log { get; private set; }
+
+        private readonly Harmony _harmony;
 
         [Init]
         public Plugin(IPALogger logger, Zenjector zenjector)
         {
             Log = logger;
+
+            _harmony = new Harmony(kHarmonyId);
+
             zenjector.OnApp<SiraLocalizerCoreInstaller>();
             zenjector.OnMenu<SiraLocalizerUIInstaller>();
         }
 
-        [OnEnable, OnDisable]
-        public void OnState() { }
+        [OnEnable]
+        public void OnEnable()
+        {
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            _harmony.UnpatchAll(kHarmonyId);
+        }
     }
 }
