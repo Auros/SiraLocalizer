54ee09b0cec6fd9126d096f83f3cb3b5cbf0d059
Nicolas Gnyra <nicolas@gnyra.com>
2021-01-06 15:41:55 -0500

    Fix LEVEL FAILED not being translatable

---
+++ SiraLocalizer/HarmonyPatches/LevelFailedController.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using IPA.Utilities;
+using Polyglot;
+using TMPro;
+
+namespace SiraLocalizer.HarmonyPatches
+{
+    [HarmonyPatch(typeof(StandardLevelFailedController), "Start")]
+    internal class StandardLevelFailedController_Start : LevelFailedController_Start
+    {
+        public static void Postfix(LevelFailedTextEffect ____levelFailedTextEffect)
+        {
+            AddLocalizedText(____levelFailedTextEffect);
+        }
+    }
+
+    [HarmonyPatch(typeof(MissionLevelFailedController), "Start")]
+    internal class MissionLevelFailedController_Start : LevelFailedController_Start
+    {
+        public static void Postfix(LevelFailedTextEffect ____levelFailedTextEffect)
+        {
+            AddLocalizedText(____levelFailedTextEffect);
+        }
+    }
+
+    [HarmonyPatch(typeof(MultiplayerLocalActiveLevelFailController), "Start")]
+    internal class MultiplayerLocalActiveLevelFailController_Start : LevelFailedController_Start
+    {
+        public static void Postfix(LevelFailedTextEffect ____levelFailedTextEffect)
+        {
+            AddLocalizedText(____levelFailedTextEffect);
+        }
+    }
+
+    internal class LevelFailedController_Start
+    {
+        protected static void AddLocalizedText(LevelFailedTextEffect levelFailedTextEffect)
+        {
+            bool wasActive = levelFailedTextEffect.gameObject.activeSelf;
+            levelFailedTextEffect.gameObject.SetActive(false);
+
+            TextMeshPro text = levelFailedTextEffect.GetComponent<TextMeshPro>();
+            text.fontStyle |= FontStyles.UpperCase;
+            text.lineSpacing = -40;
+
+            LocalizedTextMeshPro localizedText = levelFailedTextEffect.gameObject.AddComponent<LocalizedTextMeshPro>();
+            localizedText.SetField<LocalizedTextComponent<TextMeshPro>, TextMeshPro>("localizedComponent", text);
+            localizedText.Key = "LEVEL_FAILED";
+
+            levelFailedTextEffect.gameObject.SetActive(wasActive);
+        }
+    }
+}
