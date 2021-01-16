c1edc788acdba4c99c2276ebb59f3db78ef798eb
Nicolas Gnyra <nicolas@gnyra.com>
2021-01-15 23:33:53 -0500

    Add corrections and stuff to export

---
+++ SiraLocalizer/LocalizationExporter.cs
@@ -0,0 +1,86 @@
+﻿using IPA.Utilities;
+using Polyglot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SiraLocalizer
+{
+    internal class LocalizationExporter
+    {
+        // keys that aren't actually used
+        private static readonly string[] kLocalizationKeyIgnoreList = { "MP_MISSING_SONG_ENTITLEMENT", "MODIFIER_PRO_MODE", "MODIFIER_PRO_MODE_HINT", "LANGUAGE_EN", "LANGUAGE_SC" };
+
+        // keys added by SiraLocalizer
+        private static readonly (string, string)[] kAdditionalKeys = new[]
+        {
+            ("MENU_LANGUAGE_THIS", "English"),
+            ("MENU_TRANSLATED_BY", "Translated by"),
+            ("LEVEL_FAILED", " Level\nFailed"),
+            ("FLYING_TEXT_MISS", "Miss"),
+            ("LABEL_COMBO", "Combo")
+        };
+
+        // because there's typos and weirdness
+        private static readonly Dictionary<string, (string find, string replace)> kCorrections = new Dictionary<string, (string, string)>
+        {
+            { "MISSION_HELP_MIN_HANDS_MOVEMENT_TITLE", (".</color>", "</color>.") },
+            { "MISSION_HELP_MAX_HANDS_MOVEMENT", (".</color>", "</color>.") },
+            { "LABEL_MULTIPLAYER_MAINTENANCE_UPCOMING", ("maintatance", "maintenance") },
+            { "HINT_OPTIONS_BUTTON", ("Settings", "Options") },
+            { "SETTINGS_OCULUS_MRC_INFO", ("Mixed Reality Capture Setup Guide", "Getting Started With Mixed Reality Capture") }
+        };
+
+        public void DumpBaseGameLocalization()
+        {
+            string filePath = Path.Combine(UnityGame.InstallPath, "localization.csv");
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    LocalizationAsset baseGameAsset = Localization.Instance.InputFiles.First();
+                    List<List<string>> rows = CsvReader.Parse(baseGameAsset.TextAsset.text);
+
+                    writer.WriteLine("Polyglot,100,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+
+                    foreach (List<string> row in rows.SkipWhile(r => r[0] != "Polyglot").Skip(1))
+                    {
+                        if (string.IsNullOrEmpty(row[0]) || kLocalizationKeyIgnoreList.Contains(row[0])) continue;
+
+                        string key     = row.ElementAtOrDefault(0);
+                        string context = row.ElementAtOrDefault(1);
+                        string english = row.ElementAtOrDefault(2).TrimEnd();
+
+                        if (kCorrections.TryGetValue(key, out var rule))
+                        {
+                            if (!english.Contains(rule.find)) Plugin.Log.Warn($"Rule for '{key}' ('{rule.find}' -> '{rule.replace}') won't do anything on '{english}'");
+
+                            english = english.Replace(rule.find, rule.replace);
+                        }
+
+                        writer.WriteLine($"{EscapeCsvValue(key)},{EscapeCsvValue(context)},{EscapeCsvValue(english)},,,,,,,,,,,,,,,,,,,,,,,,,,,");
+                    }
+
+                    foreach ((string key, string value) in kAdditionalKeys)
+                    {
+                        writer.WriteLine($"{EscapeCsvValue(key)},,{EscapeCsvValue(value)},,,,,,,,,,,,,,,,,,,,,,,,,,,");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log.Error("Could not dump base game localization: " + ex);
+            }
+        }
+
+        private string EscapeCsvValue(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+            if (!value.Contains(',') && !value.Contains('"') && !value.Contains('\n')) return value;
+
+            return "\"" + value.Replace("\"", "\"\"") + "\"";
+        }
+    }
+}
--- SiraLocalizer/Plugin.cs
+++ SiraLocalizer/Plugin.cs
@@ -8,11 +8,6 @@ using Conf = IPA.Config.Config;
 using IPALogger = IPA.Logging.Logger;
 using System;
 using System.Linq;
-using Polyglot;
-using System.IO;
-using IPA.Utilities;
-using System.Text;
-using System.Collections.Generic;
 
 namespace SiraLocalizer
 {
@@ -21,8 +16,7 @@ namespace SiraLocalizer
     {
         private const string kHarmonyId = "pro.sira.siralocalizer";
 
-        // keys that aren't actually used
-        private static readonly string[] kLocalizationKeyIgnoreList = { "MP_MISSING_SONG_ENTITLEMENT", "MODIFIER_PRO_MODE", "MODIFIER_PRO_MODE_HINT", "LANGUAGE_EN", "LANGUAGE_SC" };
+        
 
         internal static IPALogger Log { get; private set; }
 
@@ -35,7 +29,7 @@ namespace SiraLocalizer
 
             _harmony = new Harmony(kHarmonyId);
 
-            if (Environment.GetCommandLineArgs().Contains("--dump-localization")) DumpBaseGameLocalization();
+            if (Environment.GetCommandLineArgs().Contains("--dump-localization")) new LocalizationExporter().DumpBaseGameLocalization();
 
             zenjector.OnApp<SiraLocalizerCoreInstaller>().WithParameters(conf.Generated<Config>());
             zenjector.OnMenu<SiraLocalizerUIInstaller>();
@@ -52,45 +46,6 @@ namespace SiraLocalizer
         public void OnDisable()
         {
             _harmony.UnpatchAll(kHarmonyId);
-        }
-
-        private void DumpBaseGameLocalization()
-        {
-            string filePath = Path.Combine(UnityGame.InstallPath, "localization.csv");
-
-            try
-            {
-                using (StreamWriter writer = new StreamWriter(filePath))
-                {
-                    LocalizationAsset baseGameAsset = Localization.Instance.InputFiles.First();
-                    List<List<string>> rows = CsvReader.Parse(baseGameAsset.TextAsset.text);
-
-                    writer.WriteLine("Polyglot,100,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
-
-                    foreach (List<string> row in rows.SkipWhile(r => r[0] != "Polyglot").Skip(1))
-                    {
-                        if (string.IsNullOrEmpty(row[0]) || kLocalizationKeyIgnoreList.Contains(row[0])) continue;
-
-                        string key     = EscapeCsvValue(row.ElementAtOrDefault(0));
-                        string context = EscapeCsvValue(row.ElementAtOrDefault(1));
-                        string english = EscapeCsvValue(row.ElementAtOrDefault(2));
-
-                        writer.WriteLine($"{key},{context},{english},,,,,,,,,,,,,,,,,,,,,,,,,,,");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Could not dump base game localization: " + ex);
-            }
-        }
-
-        private string EscapeCsvValue(string value)
-        {
-            if (string.IsNullOrEmpty(value)) return null;
-            if (!value.Contains(',') && !value.Contains('"') && !value.Contains('\n')) return value;
-
-            return "\"" + value.Replace("\"", "\"\"") + "\"";
         }
     }
 }
