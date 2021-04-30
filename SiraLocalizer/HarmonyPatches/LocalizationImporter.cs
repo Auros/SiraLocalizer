d631f6e6a035c8823fad184d108c99a0798a1617
Nicolas Gnyra <nicolas@gnyra.com>
2021-04-29 20:36:38 -0400

    Add patch that removes debug columns from ImportTextFile

---
+++ SiraLocalizer/HarmonyPatches/LocalizationImporter.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Polyglot;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SiraLocalizer.HarmonyPatches
+{
+    /// <summary>
+    /// This patch gets rid of the debug code added to <see cref="LocalizationImporter"/>'s ImportTextFile.
+    /// </summary>
+    [HarmonyPatch(typeof(LocalizationImporter), "ImportTextFile")]
+    internal static class LocalizationImporter_GetLanguages
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> instructionsList = instructions.ToList();
+            instructionsList.RemoveRange(48, 56);
+            return instructionsList.AsEnumerable();
+        }
+    }
+}
