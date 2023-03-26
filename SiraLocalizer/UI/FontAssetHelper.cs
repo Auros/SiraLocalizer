bbbca9c7afd8ff68393fb1a81d8fdda1b73e5ca4
Nicolas Gnyra <nicolas@gnyra.com>
2023-03-26 01:20:25 -0400

    Set texture and material names when copying

--- SiraLocalizer/UI/FontAssetHelper.cs
+++ SiraLocalizer/UI/FontAssetHelper.cs
@@ -1,4 +1,4 @@
-﻿using IPA.Utilities;
+using IPA.Utilities;
 using TMPro;
 using UnityEngine;
 
@@ -10,18 +10,29 @@ namespace SiraLocalizer.UI
 
         public TMP_FontAsset CopyFontAsset(TMP_FontAsset original, Material referenceMaterial, string newName = null)
         {
+            if (string.IsNullOrEmpty(newName))
+            {
+                newName = original.name;
+            }
+
             TMP_FontAsset copy = Object.Instantiate(original);
 
             // Unity doesn't copy textures when using Object.Instantiate so we have to do it manually
             Texture2D texture = original.atlasTexture;
-            var newTexture = new Texture2D(texture.width, texture.height, texture.format, texture.mipmapCount, true);
+            var newTexture = new Texture2D(texture.width, texture.height, texture.format, texture.mipmapCount, true)
+            {
+                name = $"{newName} Atlas",
+            };
             Graphics.CopyTexture(texture, newTexture);
 
-            var material = new Material(referenceMaterial);
+            var material = new Material(referenceMaterial)
+            {
+                name = $"{newName} Atlas Material",
+            };
             material.SetTexture("_MainTex", newTexture);
 
             kAtlasTextureAccessor(ref copy) = newTexture;
-            copy.name = !string.IsNullOrWhiteSpace(newName) ? newName : original.name;
+            copy.name = newName;
             copy.atlasTextures = new[] { newTexture };
             copy.material = material;
 
